2bcb9e51ed7d722fab26a9a1359fea4f8e496193
mjgupta <34043603+mjgupta@users.noreply.github.com>
2023-10-22 14:59:56 +0530

    Update comp.py

--- comp.py
+++ comp.py
@@ -2,36 +2,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-csv_file = sys.argv[1] 
+csv_file = sys.argv[1]
 
 df = pd.read_csv(csv_file)
 
 fig, ax = plt.subplots()
 
-ax.boxplot([df['X enabled'], df['X disabled']], positions=[1,2], widths=0.6,
-           boxprops=dict(facecolor='lightgreen'),
-           medianprops=dict(color='black')) 
+ax.boxplot([df['X enabled']], positions=[1], widths=0.6,
+           boxprops=dict(facecolor='lightgreen'))
+
+ax.boxplot([df['X disabled']], positions=[2], widths=0.6,
+           boxprops=dict(facecolor='lightcoral'))
 
 ax.axvline(2.5, linestyle='--', color='black')
 
-ax.boxplot([df['Y enabled'], df['Y disabled']], positions=[3,4], widths=0.6,
-           boxprops=dict(facecolor='lightgreen'),
-           medianprops=dict(color='black'))
+ax.boxplot([df['Y enabled']], positions=[3], widths=0.6,
+           boxprops=dict(facecolor='lightgreen')) 
+
+ax.boxplot([df['Y disabled']], positions=[4], widths=0.6,
+           boxprops=dict(facecolor='lightcoral'))
 
 ax.axvline(4.5, linestyle='--', color='black')
 
-ax.boxplot([df['Z enabled'], df['Z disabled']], positions=[5,6], widths=0.6,
-           boxprops=dict(facecolor='lightgreen'),
-           medianprops=dict(color='black'))
+ax.boxplot([df['Z enabled']], positions=[5], widths=0.6,
+           boxprops=dict(facecolor='lightgreen'))
+
+ax.boxplot([df['Z disabled']], positions=[6], widths=0.6, 
+           boxprops=dict(facecolor='lightcoral'))
 
 ax.set_xticks([1.5, 3.5, 5.5])
-ax.set_xticklabels(['X', 'Y', 'Z'])
+ax.set_xticklabels(['X', 'Y', 'Z'])  
 
 ax.set_ylabel('AppRx in Kbps')
 
 for i, box in enumerate(ax.artists):
-    x = i + 1
-    y = box.get_ydata()[2] 
-    ax.text(x, y, str(y), ha='center') 
+    x = i // 2 + 1
+    y = box.get_ydata()[2]
+    ax.text(x, y, str(y), ha='center')
 
 plt.show()

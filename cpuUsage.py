bd818be504c180c7a9e35da42d9b06201f14a767
mjgupta <34043603+mjgupta@users.noreply.github.com>
2023-10-24 02:11:05 +0530

    Update cpuUsage.py

--- cpuUsage.py
+++ cpuUsage.py
@@ -4,7 +4,7 @@ import numpy as np
 # Data
 groups = ['Group 1', 'Group 2']
 labels = ['Bar 1', 'Bar 2', 'Bar 3']
-values = [[47.09, 44.13, 57.8], [22.04, 19.75, 21.38]]
+values = [[47.09, 44.13, 57.8], [22.04, 19.75, 21.38]
 
 # Set the bar colors
 colors = ['lightblue', 'lightcoral', 'lightgreen']
@@ -15,18 +15,9 @@ bar_width = 0.35
 index = np.arange(num_bars)
 
 # Create the grouped bar plot
-for i in range(len(groups)):
-    plt.bar(index + i * bar_width, values[i], bar_width, label=groups[i], color=colors)
+for i in range(len(labels)):
+    plt.bar(index + i * bar_width, [value[i] for value in values], bar_width, label=labels[i], color=colors[i])
 
 # Set the x-axis labels
-plt.xlabel('Bars')
-plt.xticks(index + bar_width, labels)
-
-# Set the y-axis label
-plt.ylabel('Values')
-
-# Add a legend
-plt.legend()
-
-# Show the plot
-plt.show()
+plt.xlabel('Groups')
+plt.xticks(index

2b246905187d00deea04e343469a50a86534dbab
mjgupta <34043603+mjgupta@users.noreply.github.com>
2023-10-24 02:09:51 +0530

    Update cpuUsage.py

--- cpuUsage.py
+++ cpuUsage.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-bar_width = 0.3
-indexes = [1, 2, 3]
+# Data
+groups = ['Group 1', 'Group 2']
+labels = ['Bar 1', 'Bar 2', 'Bar 3']
+values = [[47.09, 44.13, 57.8], [22.04, 19.75, 21.38]]
 
-values1 = [47.09, 44.13, 57.8]
-values2 = [22.04, 19.75, 21.38]
+# Set the bar colors
+colors = ['lightblue', 'lightcoral', 'lightgreen']
 
-plt.bar(indexes, values1, width=bar_width, color='lightblue', label='Group 1')
-plt.bar([i + bar_width for i in indexes], values2, width=bar_width, color='lightcoral', label='Group 2')
+# Number of bars
+num_bars = len(labels)
+bar_width = 0.35
+index = np.arange(num_bars)
 
+# Create the grouped bar plot
+for i in range(len(groups)):
+    plt.bar(index + i * bar_width, values[i], bar_width, label=groups[i], color=colors)
+
+# Set the x-axis labels
 plt.xlabel('Bars')
+plt.xticks(index + bar_width, labels)
+
+# Set the y-axis label
 plt.ylabel('Values')
-plt.title('Grouped Bar Plot')
-plt.xticks([i + bar_width for i in indexes], ['Bar 1', 'Bar 2', 'Bar 3'])
+
+# Add a legend
 plt.legend()
 
+# Show the plot
 plt.show()

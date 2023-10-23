17a72ff43bcc34b59c2110eb01988cf8ebb7ca65
mjgupta <34043603+mjgupta@users.noreply.github.com>
2023-10-24 02:50:59 +0530

    legend in cpuUsage.py

--- cpuUsage.py
+++ cpuUsage.py
@@ -20,6 +20,15 @@ bars = ax.bar(x, values, color=colors, width=0.7)
 ax.bar(3, 0, color='white', width=0.7)
 for bar, value in zip(bars, values):
     ax.annotate(f'{value:.2f}', (bar.get_x() + bar.get_width() / 2, bar.get_height()), ha='center', va='bottom')
+
+legend_elements = [plt.Line2D([0], [0], marker='s', color='w', label='R', markerfacecolor='red', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', label='G', markerfacecolor='green', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', label='B', markerfacecolor='blue', markersize=10)]
+legend_labels = ['R', 'G', 'B']
+
+ax.legend(handles=legend_elements, labels=legend_labels, loc='upper right')
+
+
 # Set labels and title
 ax.set_xlabel('Size')
 ax.set_ylabel('CPU')
